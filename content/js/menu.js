093409b10db09f35aa4f64dd9db3ea1d29703acb
Toreristen <82028481+Toreristen@users.noreply.github.com>
2021-12-06 12:05:21 +0100

    js finished but no animation

--- content/js/menu.js
+++ content/js/menu.js
@@ -1,2 +1,27 @@
-console.log("js linked");
+//console.log("js linked");
 
+const menuIcon = document.querySelector("#hamburger");
+const menu = document.querySelector("#nav__menu");
+const closeIcon = document.querySelector("#closeicon");
+
+menu.style.visibility = "hidden";
+
+function toggleMenu() {
+    console.log("function works")
+    
+    if (menu.style.visibility === "hidden") {
+        menu.classList.add("showMenu");
+        menu.style.visibility = "visible";
+        hamburger.style.display = "none";
+        closeIcon.style.display = "initial";
+    }
+    else {
+        menu.style.visibility = "hidden";
+        menu.classList.remove("showMenu");
+        closeIcon.style.display = "none";
+        hamburger.style.display = "initial";
+    }
+}
+
+menuIcon.addEventListener("click", toggleMenu);
+closeIcon.addEventListener("click", toggleMenu);
